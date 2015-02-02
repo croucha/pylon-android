1f843d298447a48202379f28731f1a6675bdf931
Andre Crouch <crouch.andre@gmail.com>
2015-02-01 20:41:45 -0600

    implemented websocket connection to pylon android web application with autobahn api

--- app/src/main/java/com/pylon/activities/MainActivity.java
+++ app/src/main/java/com/pylon/activities/MainActivity.java
@@ -2,7 +2,6 @@ package com.pylon.activities;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,17 +10,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
+import de.tavendo.autobahn.WebSocketConnection;
+import de.tavendo.autobahn.WebSocketConnectionHandler;
+import de.tavendo.autobahn.WebSocketException;
 import com.pylon.R;
 
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public class MainActivity extends Activity {
-    private WebSocketClient mWebSocketClient;
+    private static final String TAG = "Websocket Test";
+
+    private final WebSocketConnection webSocketConnection = new WebSocketConnection();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +36,8 @@ public class MainActivity extends Activity {
         }
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
@@ -65,8 +60,7 @@ public class MainActivity extends Activity {
      */
     public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+        public PlaceholderFragment() {}
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -76,50 +70,47 @@ public class MainActivity extends Activity {
         }
     }
 
+    /**
+     * Define method that will make websocket connection.
+     *
+     * @return void
+     */
     private void connectWebSocket() {
-        URI uri;
+        final String wsUri = "ws://192.168.206.138:8080/pylon/echo";
+
         try {
-            uri = new URI("ws://websockethost:8080");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            return;
+            webSocketConnection.connect(wsUri, new WebSocketConnectionHandler() {
+
+                @Override
+                public void onOpen() {
+                    Log.d(TAG, "Status: Connected to " + wsUri);
+                    webSocketConnection.sendTextMessage("Pylon Android application connected");
+                }
+
+                @Override
+                public void onTextMessage(String payload) {
+                    Log.d(TAG, "Got echo: " + payload);
+                }
+
+                @Override
+                public void onClose(int code, String reason) {
+                    Log.d(TAG, "Connection lost.");
+                }
+            });
+        } catch (WebSocketException e) {
+            Log.d(TAG, e.toString());
         }
-
-        mWebSocketClient = new WebSocketClient(uri) {
-            @Override
-            public void onOpen(ServerHandshake serverHandshake) {
-                Log.i("Websocket", "Opened");
-                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
-            }
-
-            @Override
-            public void onMessage(String s) {
-                final String message = s;
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView textView = (TextView)findViewById(R.id.messages);
-                        textView.setText(textView.getText() + "\n" + message);
-                    }
-                });
-            }
-
-            @Override
-            public void onClose(int i, String s, boolean b) {
-                Log.i("Websocket", "Closed " + s);
-            }
-
-            @Override
-            public void onError(Exception e) {
-                Log.i("Websocket", "Error " + e.getMessage());
-            }
-        };
-        mWebSocketClient.connect();
     }
 
+    /**
+     * Send message from this Android's app interface to the websocket server.
+     * The fragment_main.xml holds the onClick parameter for calling this method.
+     *
+     * @param view
+     */
     public void sendMessage(View view) {
         EditText editText = (EditText)findViewById(R.id.message);
-        mWebSocketClient.send(editText.getText().toString());
+        webSocketConnection.sendTextMessage(editText.getText().toString());
         editText.setText("");
     }
 }
