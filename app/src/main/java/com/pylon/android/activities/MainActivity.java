94d5fd7e342251757abb4b3e1d37432ae17749ae
Andre Crouch <crouch.andre@gmail.com>
2015-02-09 22:39:50 -0600

    testing out display of messages received by websocket application

--- app/src/main/java/com/pylon/android/activities/MainActivity.java
+++ app/src/main/java/com/pylon/android/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.pylon.android.activities;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,20 +11,32 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 import de.tavendo.autobahn.WebSocketConnection;
 import de.tavendo.autobahn.WebSocketConnectionHandler;
 import de.tavendo.autobahn.WebSocketException;
-
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.pylon.R;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends Activity {
     private static final String TAG = "Websocket Test";
 
     private final WebSocketConnection webSocketConnection = new WebSocketConnection();
+
+    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,8 @@ public class MainActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        // Start timer to update elapsed time in text view
+        //this.startTimer();
     }
 
     @Override
@@ -98,7 +113,44 @@ public class MainActivity extends Activity {
 
                 @Override
                 public void onTextMessage(String payload) {
-                    Log.d(TAG, "Got echo: " + payload);
+                    // Define parser
+                    JsonParser jsonParser = new JsonParser();
+                    // Parse results
+                    JsonObject results = jsonParser.parse(payload).getAsJsonObject();
+                    // Define sender
+                    String sender = results.get("sender").getAsString();
+                    // Define message
+                    String message = results.get("message").getAsString();
+                    // Define received
+                    String received = results.get("received").getAsString();
+                    // Define date format
+                    // @TODO, fix format
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy");
+                    // Define date
+                    Date date = new Date();
+                    // Attempt to parse
+                    try {
+                        date = formatter.parse(received);
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    // Format time
+                    CharSequence relativeTime = DateUtils.getRelativeTimeSpanString(
+                        date.getTime(),
+                        System.currentTimeMillis(),
+                        DateUtils.SECOND_IN_MILLIS
+                    );
+                    // Define text view
+                    TextView textView = (TextView) findViewById(R.id.chatMessage);
+                    // Determine if text view is empty
+                    if(textView.getText() != "") {
+                        // Append line separator
+                        textView.append(System.getProperty("line.separator"));
+                    }
+                    // Append message to chat
+                    // @TODO look into list view and append list items with hidden tags
+                    // of the date to be parsed and updated for time elapsed
+                    textView.append(sender + ": " + message + " (" + relativeTime + ")");
                 }
 
                 @Override
@@ -109,6 +161,27 @@ public class MainActivity extends Activity {
         } catch (WebSocketException e) {
             Log.d(TAG, e.toString());
         }
+    }
+
+    // @TODO, update to parse each list item in a list view and get the date as a hidden
+    // tag and update
+    private void startTimer() {
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                // Define text view
+                TextView textView = (TextView) findViewById(R.id.chatMessage);
+                // Determine if text view is empty
+                if(textView != null && textView.getText() != "") {
+                    Pattern pattern = Pattern.compile("\\((.*?)\\)", Pattern.DOTALL);
+                    Matcher match = pattern.matcher(textView.getText());
+                    List<String> matches = new ArrayList<String>();
+                    while(match.find()){
+                        matches.add(match.group());
+                    }
+                    Log.i(TAG, matches.toString());
+                }
+            }
+        }, 0, 1000);
     }
 
     /**
