afc9d9044bd9d331242a1f30bc82d0a19a6441af
Andre Crouch <crouch.andre@gmail.com>
2015-03-01 22:00:35 -0600

    removing unused stuff

--- app/src/main/java/com/pylon/android/activities/RobotActivity.java
+++ app/src/main/java/com/pylon/android/activities/RobotActivity.java
@@ -1,25 +1,12 @@
 package com.pylon.android.activities;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import com.google.gson.Gson;
 import com.pylon.R;
 import com.pylon.android.utilities.RobotSocket;
-import com.pylon.android.utilities.Websocket;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Timer;
 import com.pylon.android.timers.RelativeTimer;
 
@@ -27,8 +14,6 @@ public class RobotActivity extends Activity {
     private static final String TAG = "Robot Activity";
 
     private RobotSocket websocket;
-
-    private Fragment placeholderFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,17 +41,5 @@ public class RobotActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         this.getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/pylon/android/timers/RelativeTimer.java
+++ app/src/main/java/com/pylon/android/timers/RelativeTimer.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import com.pylon.R;
 import com.pylon.android.utilities.Utils;
 import java.util.TimerTask;
 
